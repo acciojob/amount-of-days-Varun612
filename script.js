028709e62cdf66b9120f1f6e3e6fdfe2f55871ad
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-04-16 02:39:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function daysOfAYear(year) {
+	let days=0
+	if(year%400===0 || (year%4===0 && year%100===0)) {
+		days=366
+	}
+	else {
+		days=365
+	}
+	return days
+}
